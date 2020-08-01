90e7db2966dd3347840c2e99e85f78cbf46ef30f
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-01 16:35:40 -0600

    add self.current to forecast poro

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,20 +1,20 @@
 class Forecast
-#   attr_reader :id, :current, :hourly, :daily
+  attr_reader :id, :current, :hourly, :daily
 
-#   def initialize(forecast_info)
-#     binding.pry
-#     @id = nil
-#     @current = forecast_info[:location]
-#     @hourly = forecast_info[:location]
-#     @daily = forecast_info[:location]
-#     binding.pry
-#   end
+  def initialize(forecast_info)
+    binding.pry
+    @id = nil
+    @current = forecast_info[:location]
+    # @hourly = forecast_info[:location]
+    # @daily = forecast_info[:location]
+
+  end
 
   def self.search(location)
    forecast_json = OpenWeatherService.new.fetch_forecast_by_city(location.lat, location.long)
   #  location = Location.search(location)
-  binding.pry
    current = current(forecast_json[:current]) 
+   binding.pry
   #  hourly = hourly(forecast_json[:current]) 
   #  daily = daily(forecast_json[:current] )
   #  forecast_info = prep_forecast_info(forecast_json)
@@ -26,17 +26,17 @@ class Forecast
 
 #   # end
 
-#   def self.current(current_json)
-#     weather = current_json[:weather][0]
-#     description = weather[:description]
-#     feels_like = current_json[:feels_like]
-#     humidity = current_json[:humidity]
-#     sunrise = current_json[:sunrise]
-#     sunset = current_json[:sunset]
-#     temperature = current_json[:temp]
-#     time = current_json[:dt]
-#     uv_index = current_json[:uvi]
-#     visibility = current_json[:visibility]
-#     # binding.pry
-#   end
+  def self.current(forecast_json)
+    weather = forecast_json[:weather][0]
+    description = weather[:description]
+    feels_like = forecast_json[:feels_like]
+    humidity = forecast_json[:humidity]
+    sunrise = forecast_json[:sunrise]
+    sunset = forecast_json[:sunset]
+    temperature = forecast_json[:temp]
+    time = forecast_json[:dt]
+    uv_index = forecast_json[:uvi]
+    visibility = forecast_json[:visibility]
+    new(forecast_info)
+  end
 end
