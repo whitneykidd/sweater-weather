c60ddc6a648001b2bb72d98fa489ded67f7902b7
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-02 16:24:48 -0600

    update background poro spec, create VCRs

--- spec/poros/background_spec.rb
+++ spec/poros/background_spec.rb
@@ -12,5 +12,24 @@ describe Background do
       end
     end
 
+    it 'search(austin, co)' do
+      VCR.use_cassette('background austin') do
+        background = Background.search('austin, tx')
 
+        expected_url = "https://images.unsplash.com/photo-1559081632-cfac94fc46d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE1Mjk0NX0"
+        expect(background).to be_a(Background)
+        expect(background.url).to eq(expected_url)
+      end
+    end
+
+    it 'search(london, uk)' do
+      VCR.use_cassette('background london') do
+        background = Background.search('london, uk')
+
+        expected_url = "https://images.unsplash.com/photo-1560452992-e3c28ffc6432?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE1Mjk0NX0"
+        expect(background).to be_a(Background)
+        expect(background.url).to eq(expected_url)
+      end
+    end
+  end
 end
