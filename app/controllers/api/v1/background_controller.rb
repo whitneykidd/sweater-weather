cc05351424b5092b1971435a757d4e0dd3f0ab9a
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-02 16:44:32 -0600

    delete comments

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::BackgroundController < ApplicationController
   def index
     background = Background.search(background_params[:location])
-    # binding.pry
     render json: BackgroundSerializer.new(background)
   end
 
