594e5b84e430674d2966d964fbbf0d848212f148
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-03 13:40:40 -0600

    update user model spec to include create_user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,17 @@ describe User do
     it { should validate_presence_of(:api_key) }
     it { should validate_uniqueness_of(:api_key) }
   end
+
+  describe 'instance methods' do
+    it 'create account' do
+      user_build = build(:user)
+      user_params = { email: user_build.email,
+                    password: user_build.password,
+                    password_confirmation: user_build.password }
+
+      user = User.create_account(user_params)
+      expect(user).to be_a(User)
+      expect(user.new_record?).to eq(true)
+    end
+  end
 end
