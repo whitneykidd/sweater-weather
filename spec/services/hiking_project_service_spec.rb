3781c2d14ba79a68e596cb822cace68b1617baf5
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-03 09:38:54 -0600

    pass hiking_project_service_spec

--- spec/services/hiking_project_service_spec.rb
+++ spec/services/hiking_project_service_spec.rb
@@ -2,10 +2,28 @@ require 'rails_helper'
 
 describe HikingProjectService do
   it 'can get location for denver' do
-    # VCR.use_cassette('location denver') do
-      location = Location.find_long_lat('denver, co')
-      trails_json = HikingProjectService.new.fetch_trails(location)
-binding.pry
+    location = Location.find_long_lat('denver, co')
+    trails_json = HikingProjectService.new.fetch_trails(location)
 
+      trails_json[:trails].each do |trail|
+      expect(trail).to have_key(:name)
+      expect(trail).to have_key(:summary)
+      expect(trail).to have_key(:difficulty)
+      expect(trail).to have_key(:location)                             
     end
+    expect(trails_json[:trails].length).to eq(10)
   end
+
+  it 'can get location for austin' do
+    location = Location.find_long_lat('austin, tx')
+    trails_json = HikingProjectService.new.fetch_trails(location)
+
+      trails_json[:trails].each do |trail|
+      expect(trail).to have_key(:name)
+      expect(trail).to have_key(:summary)
+      expect(trail).to have_key(:difficulty)
+      expect(trail).to have_key(:location)                             
+    end
+    expect(trails_json[:trails].length).to eq(10)
+  end
+end
