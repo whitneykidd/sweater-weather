4a7c5a737bb9aebf2fd0972803aa5cadd7cc5b33
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-03 09:51:44 -0600

    start extracting location info in trail poro

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -8,6 +8,15 @@ class Trail
 
   def self.search(location, forecast)
     trails_json = HikingProjectService.new.fetch_trails(location)
+    forecast = forecast_info(forecast.current)
+    binding.pry
     new(trails_info)
+  end
+
+  def self.current(forecast)
+    forecast[:summary] = forecast[:description]
+    forecast[:temperature] = forecast[:temperature]
+
+    # filter(forecast, :forecast)
   end
 end
--- spec/poros/trail_spec.rb
+++ spec/poros/trail_spec.rb
@@ -4,8 +4,10 @@ describe Trail do
   describe 'class methods' do
     it 'search(denver, co)' do
       # VCR.use_cassette('location denver') do
-        trail = Trail.search('denver, co')
-        expect(trail[:]).to eq(39.738453)
-      # end
+        location = Location.find_long_lat('denver, co')
+        forecast = Forecast.search(location)
+        trail = Trail.search(location, forecast)
+        expect(trail).to eq(39.738453)
+      end
     end
 end
